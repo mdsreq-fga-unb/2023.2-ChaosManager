012ae40d556dc54682fbc99093f7c83dcd29eceb
EmersonTeles <emersonteles21@gmail.com>
2023-11-28 20:16:37 -0300

    feat: estilização da pagina inicial

--- cmweb/app/page.tsx
+++ cmweb/app/page.tsx
@@ -1,13 +1,20 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row justify-center space-x-10">
-      <Link href="/criar_campanha"><button className="bg-lime-300 p-64">Criar sala de campanha</button></Link>
-      <Link href="/acessar_campanha"><button className="bg-blue-500 p-64">Acessar sala de campanha</button></Link>
-      </div>
+    <main className="flex min-h-screen flex-row items-center justify-center gap-10">
+      <Link
+        href="/criar_campanha"
+        className="bg-lime-300 p-16 rounded-lg text-xl font-semibold text-zinc-800 ease-in-out duration-300 hover:p-20"
+      >
+        Criar sala de campanha
+      </Link>
+      <Link
+        href="/acessar_campanha"
+        className="bg-blue-500 p-16 rounded-lg text-xl  font-semibold text-zinc-800 ease-in-out duration-300 hover:p-20"
+      >
+        Acessar sala de campanha
+      </Link>
     </main>
-  )
+  );
 }
