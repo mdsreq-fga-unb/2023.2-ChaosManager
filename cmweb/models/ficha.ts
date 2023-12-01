d5ed17b76e5f3fc985203c5c3e09a74a9a578a85
Larissa Gomes Silva <97994511+larigs@users.noreply.github.com>
2023-12-01 17:48:49 +0000

    feat: complete `ficha` with news models

--- cmweb/models/ficha.ts
+++ cmweb/models/ficha.ts
@@ -1,26 +1,87 @@
 import { Magia, Item, Classes } from "@/models/item-magia";
-import { Estado, Testes } from "@/models/estado";
+import { Estado } from "@/models/estado";
 import { Arma, ArmasExistentes, armas } from "@/models/arma";
 import { Equipamento, EquipamentosExistentes, equipamentos } from "@/models/equipamento";
+import { Testes } from "@/models/teste";
+import { TracoPositivo, TracosPositivos, tracosPos } from "@/models/traco-positivo";
+import { TracoNegativo, TracosNegativos, tracosNeg } from "@/models/traco-negativo";
 
 export class Ficha {
-    dados: Dados;
     NPC:boolean;
-    recursos: Recursos;
-    atributos: Atributos;
     pesoCarregado: number = 0;
 
+    tracosPositivos: TracoPositivo[] = [];
+    tracosNegativos: TracoNegativo[] = [];
     estados: Estado[] = [];
     magias: Magia[] = [];
     armas: Arma[] = [];
     equipamentos: Equipamento[] = [];
     itens: Item[] = [];
+
+    dados: Dados = {
+        nomeUsuario: "", 
+        nomeJogador: "", 
+        raca: "", 
+        profissao: "", 
+        idade: "", 
+        genero: "", 
+        historia: "", 
+        descricao: "", 
+        notas: [""]
+    };
+
+    recursos: Recursos= {
+        PV: 0,
+        PdA: 0,
+        PE: 0,
+        Exp: 0,
+        Dinheiro: 0,
+    };
+
+    atributos: Atributos= {
+        Vigor: 0,
+        Habilidade: 0,
+        Percepcao: 0,
+        Inteligencia: 0,
+        Dominio: 0,
+    };
   
-    constructor(dados: Dados, NPC:boolean, recursos: Recursos, atributos: Atributos) {
-        this.dados = dados;
+    constructor(NPC:boolean) {
         this.NPC = NPC;
-        this.recursos = recursos;
-        this.atributos = atributos;
+    }
+    
+    Dados(dados: string[], notas: string[]): void{ //passei como array os parametros senão ficaria mto grande
+        this.dados = {
+            nomeUsuario: dados[0],
+            nomeJogador: dados[1],
+            raca: dados[2],
+            profissao: dados[3],
+            idade: dados[4],
+            genero: dados[5],
+            historia: dados[6],
+            descricao: dados[7],
+            notas: notas,
+        }
+    }
+
+    Recursos(recursos: number[]):void{
+        this.recursos = {
+            PV: recursos[0],
+            PdA: recursos[1],
+            PE: recursos[2],
+            Exp: recursos[3],
+            Dinheiro: recursos[4],
+        }
+    }
+    
+    Atributos(atributos: number[]):void{
+        this.atributos = {
+            Vigor: atributos[0],
+            Habilidade: atributos[1],
+            Percepcao: atributos[2],
+            Inteligencia: atributos[3],
+            Dominio: atributos[4],
+        }
     }
 
     Capacidade():number{
@@ -32,6 +93,7 @@ export class Ficha {
     }
 
     penalidadeEstado(teste: Testes):number{
+        // testar !!!!
         let count = 0;
         this.estados.forEach(estado => {
             if(estado.afetaTeste(teste)) count += estado.valor;
@@ -39,8 +101,19 @@ export class Ficha {
         return count;
     }
 
-    Forca():number{
+    penalidadeTraco(teste: Testes):number{
+        // testar !!!!
+        let count = 0;
+        this.tracosPositivos.forEach(traco => {
+            if(traco.afetaTeste(teste)) count += traco.valor;
+        });
+        this.tracosNegativos.forEach(traco => {
+            if(traco.afetaTeste(teste)) count -= traco.valor;
+        });
+        return count;
+    }
 
+    Forca():number{
         return this.atributos.Vigor + this.atributos.Habilidade;
     }
 
@@ -80,6 +153,26 @@ export class Ficha {
         return this.atributos.Dominio + this.atributos.Percepcao;
     }
 
+    addTracoPositivo(traco: TracosPositivos, valor: number):number{
+        //precisa testar !!!!!!!
+        if(this.tracosNegativos.length > 0 && this.tracosNegativos.find((element) => tracosPos[traco].id == tracosNeg[element.traco].id)) return -1; // verifica se na lista de negativos tem algum antagônic, se retornar sim retorna -1
+        
+        this.tracosPositivos.push(
+            new TracoPositivo(traco, valor)
+        );
+        return 0;
+    }
+
+    addTracoNegativo(traco: TracosNegativos, valor: number):number{
+        //precisa testar !!!!!!!
+        if(this.tracosPositivos.length > 0 && this.tracosPositivos.find((element) => tracosNeg[traco].id == tracosPos[element.traco].id)) return -1; // verifica se na lista de negativos tem algum antagônic, se retornar sim retorna -1
+        
+        this.tracosNegativos.push(
+            new TracoNegativo(traco, valor)
+        );
+        return 0;
+    }
+
     addEstado(nome: string, valor: number):void{
         this.estados.push(
             new Estado(nome, valor)
@@ -99,6 +192,7 @@ export class Ficha {
         );
     }
     addArma(nome: string, mod_dano: number, teste: Testes, peso: number):void{    
+        // add verificação de o nome igual a algum já existente
         this.pesoCarregado += peso;
         this.armas.push(
             new Arma(nome, mod_dano, teste, peso)
@@ -113,6 +207,7 @@ export class Ficha {
     }
 
     addEquipamento(nome: string, PdA: number, peso: number):void{
+        // add verificação de o nome igual a algum já existente 
         this.pesoCarregado += peso;
         this.equipamentos.push(
             new Equipamento(nome, PdA, peso)
@@ -132,6 +227,14 @@ export class Ficha {
         this.pesoCarregado += this.itens[indice].peso - temp;
         
         if(this.itens[indice].qtd >= 0) this.removeItem(indice);
+    }
+
+    removeTracoPositivo(indice:number):void{
+        this.tracosPositivos.splice(indice, 1);
+    }
+
+    removeTracoNegativo(indice:number):void{
+        this.tracosNegativos.splice(indice, 1);
     }
 
     removeEstado(indice:number):void{
@@ -159,7 +262,7 @@ export class Ficha {
     
 }
 
-export interface Dados {
+interface Dados {
     nomeUsuario: string;
     nomeJogador: string;
     raca: string;
@@ -171,7 +274,7 @@ export interface Dados {
     notas: string[];
 }
 
-export interface Atributos {
+interface Atributos {
     Vigor: number;
     Habilidade: number;
     Percepcao: number;
@@ -179,24 +282,10 @@ export interface Atributos {
     Dominio: number;
 }
 
-export interface Recursos {
+interface Recursos {
     PV: number;
     PdA: number;
     PE: number;
     Exp: number;
     Dinheiro: number;
 }
-
-enum Traços {
-    Força,
-    "Resistência Física",
-    "Resistência Mental",
-    Sobrevivência,
-    Agilidade,
-    Destreza,
-    Competência,
-    Criatividade,
-    Manipulação,
-    Sorte,
-}
-
