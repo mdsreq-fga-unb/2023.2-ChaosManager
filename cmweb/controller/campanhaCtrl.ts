5cdddccf281741898b067ef60dc892aae1b79010
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-12-03 20:18:17 -0300

    fix: remove database

--- cmweb/controller/campanhaCtrl.ts
+++ cmweb/controller/campanhaCtrl.ts
@@ -1,14 +1,9 @@
 'use server'
 
-import { db } from "@/database/db"
-import { Campanha } from "@/models/campanha";
-
 export async function criarCampanha(formData: FormData) {
-    const database = new db();
-    database.createCampanha(new Campanha(formData.get('nome'), formData.get('historia'), formData.get('senha_mestre')));
+    // ...
 }
 
 export async function acessarCampanha(formData: FormData) {
-    const database = new db();
-    database.createCampanha(new Campanha(formData.get('nome'), formData.get('historia'), formData.get('senha_mestre')));
+    // ...
 }
