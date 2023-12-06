18c2431df2b4c6ac13dc680bcd966a56f4d742ac
Larissa Gomes Silva <97994511+larigs@users.noreply.github.com>
2023-12-06 23:40:59 +0000

    feat: função para acessar o banco

--- cmweb/database/database.ts
+++ cmweb/database/database.ts
@@ -1,5 +1,43 @@
-import { createClient } from '@supabase/supabase-js'
+export default async function connection() {
+  const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// Create a single supabase client for interacting with your database
-export const supabase = createClient('https://ophficmxowztullsncau.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waGZpY214b3d6dHVsbHNuY2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE2NTQ0NDcsImV4cCI6MjAxNzIzMDQ0N30.fLyQfAbh-hUP0NHHpN47F0RmrtMCFqbkXOK5mAs_6rg')
+  const uri = process.env.DATABASE_URL;
 
+  const dbName = process.env.DB_NAME;
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  await client.connect();
+  const db = await client.db(dbName);
+  return db;
+}
+
+export async function testDB() {
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+
+  const uri = process.env.DATABASE_URL;
+
+  const dbName = process.env.DB_NAME;
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
