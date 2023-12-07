2644326ee25adcc7abfe785b4f547fea71f28477
Larissa Gomes <larissagomes2silva@gmail.com>
2023-12-07 00:42:17 -0300

    feat: adicionando view de teste PUT

--- cmweb/app/dev_tests/page.tsx
+++ cmweb/app/dev_tests/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import GetDB from '../Components/Tests/GET/GetDB';
 import PostDB from '../Components/Tests/POST/PostDB';
+import PutDB from '../Components/Tests/PUT/PutDB';
 import styles from './dev.module.css';
 
 export default function GetAndPostDBPage() {
@@ -8,6 +9,7 @@ export default function GetAndPostDBPage() {
     <div className={styles.container}>
       <GetDB />
       <PostDB />
+      <PutDB />
     </div>
   );
 }
