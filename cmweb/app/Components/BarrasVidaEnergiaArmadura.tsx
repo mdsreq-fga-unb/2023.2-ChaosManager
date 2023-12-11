373ed9d7b47b8517b30cd5928462136607ae7cf5
Ricardo <lrsj2003@gmail.com>
2023-12-10 21:15:58 -0300

    tracos e estados na ficha

--- cmweb/app/Components/BarrasVidaEnergiaArmadura.tsx
+++ cmweb/app/Components/BarrasVidaEnergiaArmadura.tsx
@@ -1,11 +1,11 @@
-export default function BarrasVidaEnergiaArmadura({ value }: { value: number }) {
+export default function BarrasVidaEnergiaArmadura({ vida, vida_max }: { vida: number, vida_max: Function }) {
     // TODO: Pra isso aqui funcionar direito precisa do valor maximo de cada status na na model
     return (
         <div className="flex max-w-md mx-auto mt-1">
             <div className="bg-gray-200 w-full">
-                <div className={`h-full w-${value} bg-green-600`}></div>
+                <div className={`h-full w-${vida} bg-green-600`}></div>
             </div>
-            <strong className="bg-gray-200">{value}/Max</strong>
+            <strong className="bg-gray-200">{vida}/{vida_max()}</strong>
         </div>
     );
 }
