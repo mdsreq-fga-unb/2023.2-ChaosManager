e452c0b184be74b10898290bb8c06dc7c9279a53
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-12-11 22:57:23 -0300

    fix: tela principal

--- cmweb/app/campanha/[nome]/jogador/page.tsx
+++ cmweb/app/campanha/[nome]/jogador/page.tsx
@@ -1,45 +1,12 @@
 'use client'
-import { Campanha, Find } from '@/models/campanha';
-import { Ficha } from '@/models/ficha';
-import { useRouter, useSearchParams  } from 'next/navigation'
-import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation'
 
 export default function Jogador({ params }: { params: { nome: string } }) {
-  const [fichasJogador, setFichasJogador] = useState<Ficha[]>([]);
-
-  useEffect(() => {
-    dadosCampanha();
-  }, []);
-
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const typeParam = searchParams.get("type");
-  
-  const dadosCampanha = async () => {
-    try {
-      const data = await Find.findData(params.nome);
-      const { status, message, camp, result } = data;
-      
-      if (result && Array.isArray(result.fichasJogador)) {
-        setFichasJogador(result.fichasJogador);
-      }
-      
-    } catch (error) {
-      console.log(error)
-    }
-  }
     return (
       <div>
         <h1>Sala do jogador</h1>
         <button onClick={() => router.push(`/campanha/${params.nome}/ficha/edit`)}>Criar Ficha</button>
-        <div>
-        <h2>Lista de fichas</h2>
-        <ul>
-        {fichasJogador.filter(ficha => ficha.NPC === false).map((item, index) => (
-          <li key={index}><button onClick={() => {router.push(`/campanha/${params.nome}/ficha/edit?fichaId=${item._id}&type=jogador`)}}>{item.dados.nomeJogador}</button></li>
-        ))}
-        </ul>
-      </div>
       </div>
     );
   }
