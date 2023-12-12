cf08e6484834a933f47c2238ff301ed853270eda
Ricardo <lrsj2003@gmail.com>
2023-12-12 02:37:10 -0300

    fix: desabilita tracos

--- cmweb/app/Components/TabelaTracos.tsx
+++ cmweb/app/Components/TabelaTracos.tsx
@@ -12,20 +12,20 @@ export default function TabelaTracos({ tracosNegativos, tracosPositivos }: { tra
                 <thead className="text-xs uppercase bg-gray-700 text-gray-400">
                     <tr>
                         <th scope="col" className="px-6 py-3"> Traço</th>
-                        <th scope="col" className="px-6 py-3"> Testes afetados</th>
+                        {/* <th scope="col" className="px-6 py-3"> Testes afetados</th> */}
                     </tr>
                 </thead>
                 <tbody>
                     {tracosNegativos.map((traco, index) => (
                         <tr className="bg-gray-800 border-gray-700 hover:bg-gray-600" key={index}>
                             <td className="w-4 p-4">{traco.traco}</td>
-                            {/* <td className="w-4 p-4">{[-]traco.testes_afetados.join(', ')}</td> */}
+                            {/* <td className="w-4 p-4">{"[-]" + traco.testes_afetados.join(', ')}</td> */}
                         </tr>
                     ))}
                     {tracosPositivos.map((traco, index) => (
                         <tr className="bg-gray-800 border-gray-700 hover:bg-gray-600" key={index}>
                             <td className="w-4 p-4">{traco.traco}</td>
-                            {/* <td className="w-4 p-4">[+]{traco.testes_afetados.join(', ')}</td> */}
+                            {/* <td className="w-4 p-4">{"[+]" + traco.testes_afetados.join(', ')}</td> */}
                         </tr>
                     ))}
                 </tbody>
