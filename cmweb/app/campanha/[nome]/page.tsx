1675d1dfab339ed606d6aef864b1088530cd75f2
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-12-12 00:11:53 -0300

    fix: tela principal e de jogador

--- cmweb/app/campanha/[nome]/page.tsx
+++ cmweb/app/campanha/[nome]/page.tsx
@@ -1,56 +1,28 @@
 'use client'
 import { Campanha, Find } from '@/models/campanha';
 import { Ficha } from '@/models/ficha';
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams  } from 'next/navigation'
 import Link from 'next/link'
 import { useState, useEffect } from 'react';
 
 
-export default function CampanhaPage({ params }: { params: { nome: string } }) {
+export default function CampanhaPage({ params }: { params: { nome: string, id: string  } }) {
   
   const [senhaMestre, setSenhaMestre] = useState('');
   const [senhaJogador, setSenhaJogador] = useState('');
   const [fichasJogador, setFichasJogador] = useState<Ficha[]>([]); 
   const [fichasNpc, setFichasNpc] = useState<Ficha[]>([]);
+  const [ficha, setFicha] = useState<Ficha>();
+  const [campanha, SetCampanha] = useState<Campanha | null>(null);
 
   useEffect(() => {
     dadosCampanha();
   }, []);
 
   const router = useRouter();
-
-  const addItemJogador = (lista: Ficha[], item: Ficha) => {
-    setFichasJogador([...lista, item]);
-  };
-  /* 
-   const updateItemJogador = (lista: Ficha[], index: number, item: Ficha) => {
-    const newLista = [...lista];
-    newLista[index] = item;
-    setFichasJogador(newLista);
-  };
-   
-   const deleteItemJogador = (lista: Ficha[], index: number) => {
-    const newLista = [...lista];
-    newLista.splice(index, 1);
-    setFichasJogador(newLista);
-  };
-*/
-  const addItemNpc = (lista: Ficha[], item: Ficha) => {
-    setFichasNpc([...lista, item]);
-  };
-/*   
-   const updateItemNpc = (lista: Ficha[], index: number, item: Ficha) => {
-    const newLista = [...lista];
-    newLista[index] = item;
-    setFichasNpc(newLista);
-  };
-   
-   const deleteItemNpc = (lista: Ficha[], index: number) => {
-    const newLista = [...lista];
-    newLista.splice(index, 1);
-    setFichasNpc(newLista);
-  };
-  */
+  const searchParams = useSearchParams();
+  const typeParam = searchParams.get("type");
+  
   const dadosCampanha = async () => {
     try {
       const data = await Find.findData(params.nome);
@@ -83,7 +55,7 @@ export default function CampanhaPage({ params }: { params: { nome: string } }) {
         <h2>Jogadores</h2>
         <ul>
         {fichasJogador.filter(ficha => ficha.NPC === false).map((item, index) => (
-          <li key={index}><Link href={`/campanha/${params.nome}/ficha/${item._id}`}>{item.dados.nomeJogador}</Link></li>
+          <li key={index}><button onClick={() => {router.push(`/campanha/${params.nome}/ficha/edit?fichaId=${item._id}&type=jogador`)}}>{item.dados.nomeJogador}</button></li>
         ))}
         </ul>
       </div>
@@ -92,7 +64,7 @@ export default function CampanhaPage({ params }: { params: { nome: string } }) {
         <h2>NPC</h2>
         <ul>
         {fichasNpc.filter(ficha => ficha.NPC === true).map((item, index) => (
-          <li key={index}><Link href={`/campanha/${params.nome}/ficha/${item._id}`}>{item.dados.nomeJogador}</Link></li>
+          <li key={index}><button onClick={() => {router.push(`/campanha/${params.nome}/ficha/edit?fichaId=${item._id}&type=npc`)}}>{item.dados.nomeJogador}</button></li>
         ))}
         </ul>
       </div>
